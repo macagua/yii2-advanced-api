7f8e20cde3d3428d02b3a57e57e3ef65a8e92176
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2015-10-28 13:33:20 -0430

    Fixed yii\web\Request::cookieValidationKey must be configured with a secret key.

--- common/config/main.php
+++ common/config/main.php
@@ -2,8 +2,13 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'request' => [
+            'enableCookieValidation' => true,
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'XFYe6SVN-SAc4X4YkXWsmVGkufy5UPvv',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
-        ]        
+        ]
     ],
 ];
